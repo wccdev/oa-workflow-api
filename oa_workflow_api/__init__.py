be5b95ee917b0958bca0d60f28b94f81eda86e02
Leslie Chan <leslie_chan@wochacha.com>
2023-12-18 10:07:43 +0800

    fix: Upgrade Version

--- oa_workflow_api/__init__.py
+++ oa_workflow_api/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for oa-workflow-api."""
 
 __author__ = """Leslie Chan"""
-__email__ = 'ampedee@gmail.com'
-__version__ = '0.4.0'
+__email__ = 'linkandawang@gmail.com'
+__version__ = '0.4.1'
